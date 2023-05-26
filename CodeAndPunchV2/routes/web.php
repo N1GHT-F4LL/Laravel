668e8268457f410cb9519f05b41793a36e104db9
N1ghtf4ll <tri345345@gmail.com>
2023-05-26 23:30:23 +0700

    Route: add homepage

--- CodeAndPunchV2/routes/web.php
+++ CodeAndPunchV2/routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    //return view('welcome');
-    return view('home');
-});
+//Route::get('/', function () {
+//    return view('home');
+//});
+
+Route::get('/', 'HomeController@index')->name('home');
