04116347bd3811d127664ad71904604c94e91256
N1ghtf4ll <tri345345@gmail.com>
2023-05-27 01:49:10 +0700

    remove old home routes

--- CodeAndPunchV2/routes/web.php
+++ CodeAndPunchV2/routes/web.php
@@ -15,10 +15,6 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
